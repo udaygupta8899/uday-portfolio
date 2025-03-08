5c76041ffde219b6b76c88f3bc7f0218f6214f73
udaygupta8899 <udaygupta8899@gmail.com>
2025-03-09 00:18:52 +0530

    Fixed MobileNav

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -10,16 +10,12 @@ const links = [
         path:'/'
     },
     {
-        name:'services',
-        path:'/services'
-    },
-    {
         name:'resume',
         path:'/resume'
     },
     {
-        name:'work',
-        path:'/work'
+        name:'projects',
+        path:'/projects'
     },
     {
         name:'contact',
